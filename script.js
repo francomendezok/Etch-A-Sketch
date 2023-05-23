4e28de98a8065113d7678c7d3471f9db0be5da3c
francomendezok <125481413+francomendezok@users.noreply.github.com>
2023-05-23 16:21:31 -0300

    Input and Button. Need to fix grid layout

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
 const container = document.querySelector(".container");
-const submit = document.getElementById("submit").value
-console.log(submit);
+const alert = document.querySelector(".alert");
+const button = document.querySelector("button");
 
-function getValue () {
+  
 
+button.addEventListener("click",getValue);
+
+function getValue() {
+  var value = document.getElementById("submit").value;
+  
+  if (value > 64) {
+    alert.style.display = "block";
+  }
+  if (value < 64) {
+    alert.style.display = "none";
+    console.log("Value: " + value);
+    printDiv(value);
+    value = "";
+  }
 }
 
-submit.addEventListener("click", getValue);
+  
 
-
-for (let i = 0; i < size * size; i++) {
-    const div = document.createElement('div');
-    container.appendChild(div);
+function printDiv (size) {
+    for (let i = 0; i < size * size; i++) {
+      const div = document.createElement('div');
+      container.style.display = 'grid';
+      container.style.gridTemplateColumns = `repeat(${size}, 1fr)`; // 5 columnas de ancho flexible
+      container.style.gridTemplateRows = `repeat(${size}, 1fr)`; // 5 filas de alto flexible
+      container.appendChild(div);
+    }
   }
+
+  //maybe i can use grid, but there must be another way around that//
 
 
 
