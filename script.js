2973e5c7f1a7a76530d9ab13596ee7317fbae0ab
francomendezok <125481413+francomendezok@users.noreply.github.com>
2023-05-26 20:31:40 -0300

    Functional and Working, but not finished yet

--- script.js
+++ script.js
@@ -1,79 +1,77 @@
 const container = document.querySelector(".container");
-const alert = document.querySelector(".alert");
-const button = document.querySelector("#send");
+const sizeSlider = document.getElementById('sizeSlider');
+const rangeValue = document.getElementById('rangeValue');
+
+
 
 // SELECT HOVER //
-
 const color = document.querySelector(".color");
 const black = document.querySelector(".black");
 const greyScale = document.querySelector(".grey-scale");
 const rainbow = document.querySelector(".rainbow");
 const erase = document.querySelector(".erase");
 const hovers = document.querySelectorAll(".select-hover")
+//________________//
 
+// Input to update divs when range is changed //
+sizeSlider.addEventListener('input', function() {
+  const value = sizeSlider.value;
+  rangeValue.textContent = value + " x " + value;
+  printDivs(value);
+});
 
-function getValue() {
-  var value = document.getElementById("submit").value;
-   
-  if (value > 64) {
-    alert.style.display = "inline";
-    alert.style.margin = "10px";
-    alert.style.padding = "0px"
+// Print Divs that the user selected //
+function printDivs(numDivs) {
+  // Clean container
+  container.innerHTML = '';
+
+  for (let i = 0; i < numDivs * numDivs; i++) {
+    const div = document.createElement('div');
+    container.style.display = 'grid';
+    container.style.gridTemplateColumns = `repeat(${numDivs}, 1fr)`; // 5 columns
+    container.style.gridTemplateRows = `repeat(${numDivs}, 1fr)`; // 5 rows
+    div.classList.add('box');
+    container.appendChild(div);
   }
-  if (value < 64) {
-    alert.style.display = "none";
-    printDiv(value);
-  }  
 }
+  
 
-function printDiv (size) {
-    container.textContent = ''; // Clear the container, remove the old divs
-    for (let i = 0; i < size * size; i++) {
-      const div = document.createElement('div');
-      container.style.display = 'grid';
-      container.style.gridTemplateColumns = `repeat(${size}, 1fr)`; // 5 columnas de ancho flexible
-      container.style.gridTemplateRows = `repeat(${size}, 1fr)`; // 5 filas de alto flexible
-      div.classList.add("box");
-      container.appendChild(div);
+var className;
+//with the class name, apply the hover effect//
+document.addEventListener('DOMContentLoaded', function() {
+  const divs = document.querySelectorAll('.box');
+  divs.forEach(div => {
+    div.addEventListener('mouseover', function() {
+      switch (className) {
+        case "black":
+          div.style.backgroundColor = 'black';
+          break;
+        case "color":
+          div.style.backgroundColor = 'red';
+          break;
       
-    }
-  }
-  button.addEventListener("click",getValue);
-  
-  window.addEventListener("keypress", function(event) {
-    // If the user presses the "Enter" key on the keyboard
-    if (event.key === "Enter") {
-      // Cancel the default action, if needed
-      event.preventDefault();
-      // Trigger the button element with a click
-      document.querySelector("#send").click();
-    }
+        default:
+          div.style.backgroundColor = 'black';
+          break;
+      }
+    });
   });
-
   // Get Class Name //
-
-  
   hovers.forEach(option => option.addEventListener("click", function getClassName () {
-    //get class name// 
-    const className = option.classList[1];
-    console.log(className);
-}))
+    className = option.classList[1];
+    return className;
+  }))
+});
 
-    //with the class name, apply the hover effect//
 
-    const divs = document.querySelectorAll(".box");
-    divs.forEach(div => {
-      div.addEventListener("click", function () {
-        div.style.backgroundColor = "black";
-      });
-  });
   
+  // Default Size //
+  printDivs(32);
+
+
 
 
 
 
 
   
-
-  // Use a bar to select the size (4x4, 16x16, etc)
-
